a118b87f5313fbd23eb414f0c0626272f763b741
samuelmswong <samuelmswong@gmail.com>
2022-02-14 18:39:18 +0000

    extended demo

--- sim/demo.m
+++ sim/demo.m
@@ -1,6 +1,9 @@
 addi x3, x0, -108
 xori x4, x3, -1
-slli x4, x3, 27
-slliw x5, x3, 27
+slli x4, x3, 19
+slliw x5, x3, 19
 addiw x6, x5, 15
+addi x7, x0, 3
+sraw x8, x6, x7
+srlw x9, x6, x7
 dumpregs
